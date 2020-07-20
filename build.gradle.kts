666987e5790eccac8376aa0885b8f36977753bf6
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2020-07-19 21:59:41 -0300

    Remove use-less libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,12 @@ plugins {
     kotlin("jvm") version "1.3.72"
 }
 
-group = "com.gabriel.engine.project"
+group = "com.engine.project"
 version = "1.0"
 
 repositories {
     mavenCentral()
     mavenLocal()
-
-    jcenter()
 
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
@@ -20,9 +18,6 @@ dependencies {
 
     compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
     compileOnly("com.github.azbh111:craftbukkit-1.8.8:R")
-
-    implementation("org.koin:koin-core:2.1.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
 }
 
 tasks {
